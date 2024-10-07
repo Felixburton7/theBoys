816b2e0e61145529ce87f72e1c089fca9a26fc00
Felix Burton <felixburton2002@gmail.com>
2024-10-07 18:40:07 +0100

    Changed title to The boys

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The Boys',
       theme: ThemeData(
         colorScheme:
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 26, 48, 123)),
